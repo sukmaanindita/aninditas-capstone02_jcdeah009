6b22a26de235a6b5fdbe95f2e1a85ff2dcb10354
Sukma Anindita <anninditas@anninditas-MacBook-Air.local>
2026-07-07 10:27:57 +0700

    Bugs Fixed : Update Data Source from Gold Stage >> business_questions.sql

--- business_questions.sql
+++ business_questions.sql
@@ -2,7 +2,7 @@
 -- (Total Valid Trip)
 SELECT
     COUNT(*) AS total_valid_trips
-FROM silver.cleaned_yellow_taxi_trip;
+FROM gold.vw_trip_enriched;
 
 -- (Total Trips and Average Metrics by Weekend vs. Weekday)
 SELECT
@@ -11,7 +11,7 @@ SELECT
     ROUND(AVG(passenger_count), 2) AS avg_passenger_count,
     ROUND(AVG(trip_distance), 2) AS avg_trip_distance,
     ROUND(AVG(total_amount), 2) AS avg_total_amount
-FROM silver.cleaned_yellow_taxi_trip
+FROM gold.vw_trip_enriched
 GROUP BY is_weekend;
 
 
@@ -23,7 +23,7 @@ SELECT
     ROUND(SUM(total_amount), 2) AS total_revenue,
     ROUND(AVG(total_amount), 2) AS avg_revenue,
     round(AVG(trip_distance), 2) AS avg_trip_distance
-FROM silver.cleaned_yellow_taxi_trip
+FROM gold.vw_trip_enriched
 GROUP BY payment_type
 ORDER BY total_trips DESC
 LIMIT 1;
@@ -32,25 +32,21 @@ LIMIT 1;
 -- 2 Join and Location Analysis
 -- (Highest Average Trip Distance by Pickup Location)
 SELECT
-    pickup_zone.borough AS pickup_location,
-    ROUND(AVG(y.trip_distance), 2) AS avg_trip_distance,
-    COUNT(*) AS total_trips
-FROM silver.cleaned_yellow_taxi_trip y
-LEFT JOIN silver.taxi_zones_mapping pickup_zone
-    ON y.pulocationid = pickup_zone.locationid
-GROUP BY pickup_zone.borough
+    pickup_location,
+    ROUND(SUM(avg_trip_distance * total_trips) / SUM(total_trips), 2) AS avg_trip_distance,
+    SUM(total_trips) AS total_trips
+FROM gold.vw_zone_performance
+GROUP BY pickup_location
 ORDER BY avg_trip_distance DESC
 LIMIT 1;
 
 -- (Highest Average Revenue by Pickup Location)
 SELECT
-    pickup_zone.borough AS pickup_location,
-    ROUND(AVG(y.total_amount), 2) AS avg_revenue,
-    COUNT(*) AS total_trips
-FROM silver.cleaned_yellow_taxi_trip y
-LEFT JOIN silver.taxi_zones_mapping pickup_zone
-    ON y.pulocationid = pickup_zone.locationid
-GROUP BY pickup_zone.borough
+    pickup_location,
+    ROUND(SUM(avg_revenue * total_trips) / SUM(total_trips), 2) AS avg_revenue,
+    SUM(total_trips) AS total_trips
+FROM gold.vw_zone_performance
+GROUP BY pickup_location
 ORDER BY avg_revenue DESC
 LIMIT 1;
 
@@ -59,13 +55,12 @@ LIMIT 1;
 -- Summary of Trips by Pickup Date
 SELECT
     pickup_date,
-    COUNT(*) AS total_trips,
-    ROUND(SUM(total_amount), 2) AS total_revenue,
-    ROUND(SUM(trip_distance), 2) AS total_trip_distance,
-    ROUND(AVG(trip_duration_minutes), 2) AS avg_trip_duration_minutes
-FROM silver.cleaned_yellow_taxi_trip
-GROUP BY pickup_date
-ORDER BY pickup_date DESC
+    total_trips,
+    total_revenue,
+    total_trip_distance,
+    avg_trip_duration_minutes
+FROM gold.vw_daily_trip_summary
+ORDER BY pickup_date DESC;
 
 -- Highest Invalid Data Quality Issues by error_type
 SELECT
@@ -82,13 +77,12 @@ LIMIT 1;
 WITH pickup_location_revenue AS (
     SELECT
         y.pulocationid,
-        pickup_zone.borough AS pickup_location,
+        y.pickup_location,
+        y.pickup_zone,
         ROUND(AVG(y.total_amount), 2) AS avg_revenue,
         COUNT(*) AS total_trips
-    FROM silver.cleaned_yellow_taxi_trip y
-    LEFT JOIN silver.taxi_zones_mapping pickup_zone
-        ON y.pulocationid = pickup_zone.locationid
-    GROUP BY y.pulocationid, pickup_zone.borough
+    FROM gold.vw_trip_enriched y
+    GROUP BY y.pulocationid, y.pickup_location, y.pickup_zone
 )
 SELECT *
 FROM pickup_location_revenue
@@ -97,13 +91,11 @@ LIMIT 10;
 
 -- (Highest Pickup Location with Minimum Average Tip Amount)
 SELECT
-    pickup_zone.borough AS pickup_location,
+    y.pickup_location,
     ROUND(AVG(y.tip_amount), 2) AS avg_tip_amount,
     COUNT(*) AS total_trips
-FROM silver.cleaned_yellow_taxi_trip y
-LEFT JOIN silver.taxi_zones_mapping pickup_zone
-    ON y.pulocationid = pickup_zone.locationid
-GROUP BY pickup_zone.borough
+FROM gold.vw_trip_enriched y
+GROUP BY y.pickup_location
 ORDER BY avg_tip_amount ASC
 LIMIT 1;
 
@@ -111,9 +103,8 @@ LIMIT 1;
 WITH daily_revenue AS (
     SELECT
         pickup_date,
-        ROUND(SUM(total_amount), 2) AS total_revenue
-    FROM silver.cleaned_yellow_taxi_trip
-    GROUP BY pickup_date
+        total_revenue
+    FROM gold.vw_daily_trip_summary
 ),
 avg_revenue AS (
     SELECT
@@ -133,39 +124,35 @@ ORDER BY dr.total_revenue DESC;
 -- 5 Ranking and Window Functions
 -- Pickup Locations Ranked by Total Revenue
 SELECT
-    pickup_zone.borough AS pickup_location,
-    ROUND(SUM(y.total_amount), 2) AS total_revenue,
-    COUNT(*) AS total_trips,
-    RANK() OVER (ORDER BY SUM(y.total_amount) DESC) AS revenue_rank
-FROM silver.cleaned_yellow_taxi_trip y
-LEFT JOIN silver.taxi_zones_mapping pickup_zone
-    ON y.pulocationid = pickup_zone.locationid
-GROUP BY pickup_zone.borough
+    pickup_location,
+    ROUND(SUM(total_revenue), 2) AS total_revenue,
+    SUM(total_trips) AS total_trips,
+    RANK() OVER (ORDER BY SUM(total_revenue) DESC) AS revenue_rank
+FROM gold.vw_zone_performance
+GROUP BY pickup_location
 ORDER BY revenue_rank;
 
 -- Pickup Locations Ranked by Borough
 SELECT
-    pickup_zone.borough AS pickup_location,
-    ROUND(SUM(y.total_amount), 2) AS total_revenue,
+    pickup_location AS borough,
+    pickup_zone,
+    ROUND(SUM(total_amount), 2) AS total_revenue,
     COUNT(*) AS total_trips,
-    RANK() OVER (PARTITION BY pickup_zone.borough ORDER BY SUM(y.total_amount) DESC) AS revenue_rank_within_borough
-FROM silver.cleaned_yellow_taxi_trip y
-LEFT JOIN silver.taxi_zones_mapping pickup_zone
-    ON y.pulocationid = pickup_zone.locationid
-GROUP BY pickup_zone.borough
-ORDER BY pickup_zone.borough, revenue_rank_within_borough;
+    RANK() OVER (PARTITION BY pickup_location ORDER BY SUM(total_amount) DESC) AS revenue_rank_within_borough
+FROM gold.vw_trip_enriched
+GROUP BY pickup_location, pickup_zone
+ORDER BY pickup_location, revenue_rank_within_borough;
 
 -- Moving Average by Trip Duration (7-day Moving Average)
 WITH daily_trip_duration AS (
     SELECT
         pickup_date,
-        ROUND(AVG(trip_duration_minutes), 2) AS avg_trip_duration
-    FROM silver.cleaned_yellow_taxi_trip
-    GROUP BY pickup_date
+        avg_trip_duration_minutes AS avg_trip_duration
+    FROM gold.vw_daily_trip_summary
 )
 SELECT
     pickup_date,
     avg_trip_duration,
     ROUND(AVG(avg_trip_duration) OVER (ORDER BY pickup_date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS moving_avg_trip_duration_7_days
 FROM daily_trip_duration
-ORDER BY pickup_date;  
+ORDER BY pickup_date;
